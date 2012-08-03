c985bee21c3fa66d1974f039d86ac7a331ed9a40
Carlos <huaixian.huang@gmail.com>
2012-08-03 21:46:34 +0800

    add demo code to notify in status bar

--- src/com/carlos/tomatoclock/MainActivity.java
+++ src/com/carlos/tomatoclock/MainActivity.java
@@ -4,6 +4,9 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -33,48 +36,56 @@ public class MainActivity extends Activity {
 	static int status = 0;//tomato stop /1tomato start /2 reststop /3 reststart
 	
 	static int tomatoCount = 0;
+	
+    NotificationManager m_NotificationManager;  
 
-	Handler handler = new Handler() {
-	    @Override
-	    public void handleMessage(Message msg) {
-	    	Log.v(TAGS, "get one second message"+msg.what+" tomatoCount is: "+tomatoCount);
-	        super.handleMessage(msg);
-	        if(msg.what == 0)
-	        {
-				switch (status) {
-				case 0:
+    Notification m_Notification;  
 
-					break;
-				case 1:
-					tomatoCount -= 1;
-					if(tomatoCount < 0)
-					{
-						status = 2;
-						vibrator.vibrate(pattern, 0);
-					}
-					updateData(getString(R.string.tomato).toString(),tomatoCount);
-					break;
-				case 2:
+    PendingIntent m_PendingIntent;  
 
-					break;
-				case 3:
-					tomatoCount -= 1;
-					if(tomatoCount < 0)
-					{
-						status = 0;
-						vibrator.vibrate(pattern, 0);
-					}
-					updateData(getString(R.string.rest).toString(),tomatoCount);
-					break;
-				default:
-					break;
-				}
-				updateBtn();
-	        	
-	        }
-//	        MainActivity.this.onRestart();
-	    }
-	};
+    Intent m_Intent;  
+
+//	Handler handler = new Handler() {
+//	    @Override
+//	    public void handleMessage(Message msg) {
+//	    	Log.v(TAGS, "get one second message"+msg.what+" tomatoCount is: "+tomatoCount);
+//	        super.handleMessage(msg);
+//	        if(msg.what == 0)
+//	        {
+//				switch (status) {
+//				case 0:
+//
+//					break;
+//				case 1:
+//					tomatoCount -= 1;
+//					if(tomatoCount < 0)
+//					{
+//						status = 2;
+//						vibrator.vibrate(pattern, 0);
+//					}
+//					updateData(getString(R.string.tomato).toString(),tomatoCount);
+//					break;
+//				case 2:
+//
+//					break;
+//				case 3:
+//					tomatoCount -= 1;
+//					if(tomatoCount < 0)
+//					{
+//						status = 0;
+//						vibrator.vibrate(pattern, 0);
+//					}
+//					updateData(getString(R.string.rest).toString(),tomatoCount);
+//					break;
+//				default:
+//					break;
+//				}
+//				updateBtn();
+//	        	
+//	        }
+////	        MainActivity.this.onRestart();
+//	    }
+//	};
 	
 	
 	@Override
@@ -86,7 +97,7 @@ public class MainActivity extends Activity {
 		vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
 //		vibrator.vibrate(pattern, 0);// -1不重复，非-1为从pattern的指定下标开始重复
     	Log.v(TAGS, "timer is: "+timer);
-    	Log.v(TAGS, "handler is: "+handler);
+//    	Log.v(TAGS, "handler is: "+handler);
 
     	
 		timer = new Timer();
@@ -120,6 +131,25 @@ public class MainActivity extends Activity {
 //		intent.putExtras(bl);
 		
 		startService(intent);
+		
+        m_NotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);  
+        
+        m_Intent = new Intent(MainActivity.this, MainActivity.class);  
+
+        m_PendingIntent = PendingIntent.getActivity(MainActivity.this, 0,  
+                m_Intent, 0); 
+
+        m_Notification = new Notification();
+        
+        m_Notification.icon = R.drawable.ic_launcher;  
+
+        m_Notification.tickerText = "MainActivity通知内容........";  
+//         通知时既震动又屏幕发亮还有默认的声音 这里用的是ALL  
+        m_Notification.defaults = Notification.DEFAULT_ALL;  
+        
+        m_Notification.setLatestEventInfo(MainActivity.this, "Button4",  
+                "Button4通知", m_PendingIntent);  
+        m_NotificationManager.notify(0, m_Notification);  
 
 	}
 	private OnClickListener clickHandler= new OnClickListener() {
